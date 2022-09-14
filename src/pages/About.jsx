ede930e60b8dc7fde08c8efc7bdeb9ee1bd1bce3
meeckhout <marthe.eeckhout@gmail.com>
2022-09-14 17:06:35 +0200

    About styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import '../styles/About.scss';
+import { FaFileDownload } from 'react-icons/fa';
 
 const About = () => {
     return (
@@ -10,9 +11,10 @@ const About = () => {
             <span className="likes">
                 I like the 90s, metal music, books, Swedish meatballs, coffee and Crossfit.
             </span>
-                <a className="button" href="../assets/documents/CV_Marthe-Eeckhout.pdf" download="CV_Marthe-Eeckhout.pdf">
-                    Download Resume
-                </a>
+
+            <a className="button" href="../assets/documents/CV_Marthe-Eeckhout.pdf" download="CV_Marthe-Eeckhout.pdf">
+                <FaFileDownload /> Download Resume
+            </a>
         </div>
     );
 };
