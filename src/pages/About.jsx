b53627863b3c8438e421eb99f231943853b9b927
meeckhout <marthe.eeckhout@gmail.com>
2022-09-16 14:57:05 +0200

    About page animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,31 @@
 import '../styles/About.scss';
+import aboutme_2 from '../assets/images/aboutme_2.png';
+import meatballs_forlife from '../assets/images/meatballs_forlife.gif';
 import { FaFileDownload } from 'react-icons/fa';
 
 const About = () => {
     return (
         <div className="container">
             <span className="about">
-                I'm a front end developer and web designer based in Belgium.
+                I'm a front end developer and web designer based in Belgium, <br />
+                where I live with my two cats: Oliver and Negasonic Warhead.
             </span>
 
             <span className="likes">
                 I like the 90s, metal music, books, Swedish meatballs, coffee and Crossfit.
             </span>
 
-            <a className="button" href="../assets/documents/CV_Marthe-Eeckhout.pdf" download="CV_Marthe-Eeckhout.pdf">
-                <FaFileDownload /> Download Resume
+            <img className="meatballs" src={meatballs_forlife} alt="Swedish meatballs" />
+
+
+            <div className="spotify">
+                <span>
+                    Currently listening to:
+                </span>
+            </div>
+
+            <a className="button" href="../assets/documents/CV_Marthe-Eeckhout.png" download>
+                <FaFileDownload /> Download CV
             </a>
         </div>
     );
