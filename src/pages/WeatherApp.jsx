ec71bacc6bad85928f353f2fbe98bb4348824ad1
meeckhout <marthe.eeckhout@gmail.com>
2022-10-10 11:05:08 +0200

    Remove unused images

--- src/pages/WeatherApp.jsx
+++ src/pages/WeatherApp.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import weatherapp_1 from '../assets/images/weatherapp_1.png';
 import weatherapp_2 from '../assets/images/weatherapp_2.png';
-import Ghent from '../assets/images/Ghent.png';
-import thecityof from '../assets/images/thecityof.png';
-import font_title from '../assets/images/font_title.png';
 import '../styles/WeatherApp.scss';
 
 const WeatherApp = () => {
