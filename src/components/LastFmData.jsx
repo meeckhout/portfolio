d96c20080de4d705fabe58922e8d855d4fc35214
meeckhout <marthe.eeckhout@gmail.com>
2022-10-20 15:48:20 +0200

    Fix issue for netlify

--- src/components/LastFmData.jsx
+++ src/components/LastFmData.jsx
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from 'react';
 
 const LastFmData= () => {
-    const [lfmData, updateLfmData] = useState({});
-    const apiKey = process.env.REACT_APP_API_KEY;
-    const userName = process.env.REACT_APP_CLIENTID;
-
+    const [lfmData, updateLfmData] = useState({})
     useEffect(() => {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const userName = process.env.REACT_APP_CLIENTID;
         fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${userName}&api_key=${apiKey}&limit=1&nowplaying=true&format=json`)
             .then(response => {
                 if (response.ok) {
